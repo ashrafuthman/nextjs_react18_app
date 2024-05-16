1643cff893b0de10e129b6493cdd330f0378ae5f
ashrafuthman <ashrafelion@gmail.com>
2024-05-16 14:31:28 +0200

    dynamic routes

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
-import Link
- from "next/link";
-import Header from "./components/header";
+import Link from "next/link";
+import Header from "@/components/header";
+
 export default function Home() {
   return (
     <main>
       <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p><Link href="/about">About us</Link></p>
+      <p><Link href="/blog">Blog</Link></p>
+
     </main>
   );
 }
