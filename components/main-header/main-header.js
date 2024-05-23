c204b6028381049fe3cfb17a92a12bf7f6d57d25
ashrafuthman <ashrafelion@gmail.com>
2024-05-23 14:01:40 +0200

    organise header component

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -2,32 +2,36 @@ import Link from "next/link";
 import LogoImg from "@/assets/logo.png";
 import Image from "next/image";
 import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background/main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link href={"/"} className={classes.logo}>
-        <Image src={LogoImg} alt="Foodie Logo" priority className={classes.Image}/>
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href={"/meals"} className={classes.Link}>
-              Meals
-            </Link>
-          </li>
-          <li>
-            <Link href={"/meals/share"} className={classes.Link}>
-              Shared Meals
-            </Link>
-          </li>
-          <li>
-            <Link href={"/community"} className={classes.Link}>
-              Community
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href={"/"} className={classes.logo}>
+          <Image src={LogoImg} alt="Foodie Logo" priority className={classes.Image}/>
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href={"/meals"} className={classes.Link}>
+                Browse Meals
+              </Link>
+            </li>
+            <li>
+              <Link href={"/meals/share"} className={classes.Link}>
+                Shared Meals
+              </Link>
+            </li>
+            <li>
+              <Link href={"/community"} className={classes.Link}>
+                Community
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
