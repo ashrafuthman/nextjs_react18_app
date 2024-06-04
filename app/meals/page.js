0615b52de4c65a12c219d5f2379b59b3b2908375
Ashraf Uthman <ashrafelion@gmail.com>
2024-06-04 12:29:16 +0200

    Meal Item

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,24 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
-    return <h1>Meal Page</h1>;
+  return (
+    <header className={classes.header}>
+      <h1>
+        Delicious meals, created{' '}
+        <span className={classes.highlight}>by you</span>
+      </h1>
+      <p>
+        Choose your favorite recipe and cook it yourself. it is easy and fun!
+      </p>
+      <p className={classes.cta}>
+        <Link href="/meals/share">
+         Share Your Favorite Recipe 
+        </Link>
+      </p>
+      <main className={classes.main}>
+
+      </main>
+    </header>
+  );
 }
