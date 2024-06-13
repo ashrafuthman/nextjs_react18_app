5a3ac77e5e24f587bb26c0f3e040166def5eec7a
Ashraf Uthman <ashrafelion@gmail.com>
2024-06-13 16:53:27 +0200

    onGoing submit food recipe

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,9 @@
 import ImagePicker from '@/components/main-header/image-picker/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
